95efe1f34ec13c7d39fda068fb7b5e494e811d16
CompAndCode <66060875+compandcode@users.noreply.github.com>
2020-09-30 21:40:20 +0100

    Update firebase.js

--- React TODO Clever Programmer src/firebase.js
+++ React TODO Clever Programmer src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-        apiKey: "AIzaSyAmkoOgKCYXGsNR-afwnAVSvCJtKAa7ypg",
-        authDomain: "compandcodetodocpf.firebaseapp.com",
-        databaseURL: "https://compandcodetodocpf.firebaseio.com",
-        projectId: "compandcodetodocpf",
-        storageBucket: "compandcodetodocpf.appspot.com",
-        messagingSenderId: "426730450340",
-        appId: "1:426730450340:web:f5ddd5b9392eb697c16531",
-        measurementId: "G-5SCXQZGZBD"
+        /*ADD YOUR FIREBASE PROJECT'S CONFIG DETAILS HERE*/
 });
 
 
